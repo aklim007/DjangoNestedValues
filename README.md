d504a7244925ba8b9e692c76dc24e64a98134a6a
aklim007 <aklim007@gmail.com>
2016-03-20 21:12:45 +0500

    v0.2.1
    - GitHub
    - При проверке вхождения, текущего атрибута модели, в fields или exclude теперь учитывается, как attname так и name.
    - Из сигнатуры nested_values убрал ifnone.
    - README
    - Поправил описание типов.

---
+++ README.md
@@ -0,0 +1,84 @@
+# DjangoNestedValues
+
+## Requirements
+
+- Python >= 3.4
+- Django >= 1.9
+
+## Установка
+
+На текущий момент единственный вариант это скопировать файл с кодом в проект и вызвать setup().
+
+    from app import nestedvalues
+    nestedvalues.setup()
+
+## Использование
+
+После установки у QuerySet появляется метод nested_values.
+
+Если кратко:
+- values позволяет получить в результате запроса не экземпляр модели, а словарь
+- nested_values позволяет получить вложенные словари.
+
+Пример:
+
+    class Status(models.Model):
+        name = models.CharField(max_length=255)
+    
+    
+    class Model2(models.Model):
+        name = models.CharField(max_length=255)
+        param_1 = models.BooleanField(default=True)
+        status = models.ForeignKey(Status)
+        date_start = models.DateTimeField()
+    
+    
+    class Model3(models.Model):
+        name = models.CharField(max_length=255)
+        param_1 = models.BooleanField(default=True)
+        model2 = models.ForeignKey(Model2)
+
+
+    result = Model3.objects.all().nested_values(
+        fieldsmap=model_fields_map(
+            Model3,
+            fields={'id', 'param_1'}
+        ) + [('model2__status__name', 'status')],
+        nest={
+            'nest_model': model_fields_map(Model2, prefix='model2__')
+        }
+    ).first()
+    
+    >>> pprint(result)
+    {'id': 1,
+     'nest_model': {'date_start': datetime.datetime(2016, 3, 20, 15, 18, 19, 839941, tzinfo=<UTC>),
+                    'id': 1,
+                    'name': 'name1',
+                    'param_1': False,
+                    'status_id': 1},
+     'param_1': True,
+     'status': 'Ok'}
+
+
+model\_fields\_map - является функцией помощником, на основании переданных аргументов формируется список ('путь в орм', 'имя ключа в итоговом словаре'), при желании данный список можно сформировать самому.
+
+model\_fields\_map(model, fields=None, exclude=None, prefix='', attname=True, rename=None)
+- model - модель DjangoORM
+- fields - набор полей которые нужны в данной модели, можно указывать, как attrname так и name(данные поля отличаются для внешних ключей), при фильтрации полей модели будет происходить проверка на вхождение **in**(переданный объект должен поддерживать это).
+- exclude - набор полей которые не нужны в данной модели, остальное аналогично fields.
+- prefix - префикс пути в DjangoORM.
+- attname - использовать в качестве имени attrname или name.
+- rename - словарь для переименования полей.
+
+Сам nested_values принимает 2 аргумента (fieldsmap, nest=None)
+- список tuple описанный выше.
+- nest - словарь для описания вложенных объектов, в качестве ключа выступает имя вложенного объекта в итоговом словаре, в качестве значения может быть, как список аналогичный возвращаемому model\_fields\_map, так и экземпляр класса NV(он также может содержать вложенные объекты).
+
+Класс NV при создании принимает аргументы (fieldsmap, nest=None) - аналогичные вышеописанным и отдельный аргумент ifnone, в нём можно указать имя ключа в словаре который описывает текущий класс, и если значение по этому атрибуту будет равно None, то вместо текущего словаря будет возвращено значение None. Это необходимо чтоб не генерировать словари целиком состоящие из None, что запросто может случится если внешние ключи допускают NULL.
+
+
+## TODO
+
+- Предусмотреть возможность генерации не только вложенных словарей, но и списка вложенных словарей, в случае отношений M2M или обратных связей.
+- При использование model\_fields\_map добавить проверку, что все поля указанные в fields, exclude, rename являются действительными, это позволить избежать непредвиденного поведения когда ошибся при наборе поля, или указано поле которого уже не существует.
+- Тесты =(
---
+++ nestedvalues.py
@@ -0,0 +1,190 @@
+from copy import deepcopy
+from functools import wraps
+from itertools import count
+
+from django.db.models.query import QuerySet, BaseIterable
+
+VERSION = (0, 2, 1)
+__all__ = ['NV', 'model_fields_map']
+__title__ = 'DjangoNestedValues'
+__version__ = '.'.join(map(str, VERSION if VERSION[-1] else VERSION[:2]))
+__author__ = 'Klimenko Artyem'
+__contact__ = 'aklim007@gmail.com'
+__homepage__ = 'https://github.com/aklim007/DjangoNestedValues'
+__copyright__ = 'Клименко Артём <aklim007@gmail.com>'
+
+
+def model_fields_map(model, fields=None, exclude=None, prefix='', prefixm='', attname=True, rename=None):
+    """
+    На основании переданной модели, возвращает список tuple, содержащих путь в орм к этому полю,
+    и с каким именем оно должно войти в результат.
+    Обрабатываются только обычные поля, m2m и generic сюда не войдут.
+    ARGUMENTS:
+        :param model: модель или инстанс модели на основе которой будет формироваться список полей
+        :param None | collections.Container fields: список полей которые будут забраны из модели
+        :param None | collections.Container exclude: список полей которые не будут забираться
+        :param str prefix: ORM путь по которому будут распологаться модель в запросе
+        :param str prefixm: префикс который будет добавлен к имени поля
+        :param bool attname: использовать имя name (model) или attname(model_id) эти поля отличаются для внешних ключей
+        :param dict rename: словарь переименования полей
+        :rtype: list[tuple[str]]
+    """
+    data = []
+    rename = rename or {}
+    attribute = 'attname' if attname else 'name'
+    for f in model._meta.concrete_fields:
+        if fields and f.attname not in fields and f.name not in fields:
+            continue
+        if exclude and f.attname in exclude and f.name not in exclude:
+            continue
+        param_name = getattr(f, attribute)
+        new_param_name = rename[param_name] if param_name in rename else param_name
+        data.append(('{}{}'.format(prefix, param_name), '{}{}'.format(prefixm, new_param_name)))
+    return data
+
+
+class NV(object):
+    __slots__ = ('_fieldsmap', '_nest', '_values_list', '_lfieldsmap', '_parent', '_c', 'ifnone')
+
+    def __init__(self, fieldsmap, nest=None, ifnone=None):
+        """
+        ARGUMENTS:
+            :type fieldsmap: list[tuple[str]]
+            :type nest: nest None | dict[str, list[tuple[str]] | NV]
+            :type ifnone: None | str
+        """
+        self._fieldsmap = fieldsmap
+        #: :type: list[tuple]
+        self._lfieldsmap = []
+        #: :type: dict[str, NV | list[tuple[str]]]
+        self._nest = nest or {}
+        self._values_list = None
+        #: :type: None | NV
+        self._parent = None
+        self._c = None
+        self.ifnone = ifnone
+
+        for key, nest in self._nest.items():
+            if not isinstance(nest, NV):
+                self._nest[key] = NV(fieldsmap=nest)
+            self._nest[key]._parent = self
+
+    def _parse_value(self, value):
+        """
+        ARGUMENTS:
+            :type value: tuple
+            :rtype: dict
+        """
+        lv = {}
+        for newkey, indx in self._lfieldsmap:
+            lv[newkey] = value[indx]
+        if self.ifnone is not None and lv[self.ifnone] is None:
+            return None
+        for key, nest in self._nest.items():
+            lv[key] = nest._parse_value(value)
+        return lv
+
+    @property
+    def values_list(self):
+        """
+        Возвращает cписок полей (могут повторятся), таких каими их требуется скормить для values_list orm
+        ARGUMENTS:
+            :rtype: list[str]
+        """
+        if self._values_list is not None:
+            return self._values_list
+        for indx, key in enumerate(self._fieldsmap):
+            if isinstance(key, str):
+                self._fieldsmap[indx] = (key, key)
+        self._c = count() if self._parent is None else self._parent._c
+        v = [key[0] for key in self._fieldsmap]
+        self._lfieldsmap = [(key[1], next(self._c)) for key in self._fieldsmap]
+        for nest in self._nest.values():
+            v += nest.values_list
+        self._values_list = v
+        return self._values_list
+
+
+class NestedValuesIterable(BaseIterable):
+    """
+    Итератор который собственно и вернёт значения в нужном формате
+    """
+
+    def __iter__(self):
+        queryset = self.queryset
+        query = queryset.query
+        compiler = query.get_compiler(queryset.db)
+        _parse_value = queryset._nested_values._parse_value
+        _lfieldsmap = queryset._nested_values._lfieldsmap
+        values_list = queryset._nested_values.values_list
+        if not query.extra_select and not query.annotation_select:
+            for row in compiler.results_iter():
+                yield _parse_value(row)
+        else:
+            field_names = list(query.values_select)
+            extra_names = list(query.extra_select)
+            afields = list(query.annotation_select)
+
+            correction = len(extra_names)
+            fnames_len = len(field_names)
+            afields_start = correction + fnames_len
+            for indx, v in enumerate(values_list):
+                key, position = _lfieldsmap[indx]
+                if v in query.annotation_select:
+                    correction -= 1
+                    position = afields_start + afields.index(v)
+                else:
+                    position += correction
+                _lfieldsmap[indx] = key, position
+            for row in compiler.results_iter():
+                yield _parse_value(row)
+
+
+def _clone(self, **kwargs):
+    query = self.query.clone()
+    if self._sticky_filter:
+        query.filter_is_sticky = True
+    clone = self.__class__(model=self.model, query=query, using=self._db, hints=self._hints)
+    clone._for_write = self._for_write
+    clone._prefetch_related_lookups = self._prefetch_related_lookups[:]
+    clone._known_related_objects = self._known_related_objects
+    clone._iterable_class = self._iterable_class
+    clone._fields = self._fields
+
+    clone.__dict__.update(kwargs)
+    return clone
+
+
+def clone_wrapper(old_obj: QuerySet):
+    """
+    Деоратор функции QuerySet._clone для копирования _nested_values
+    Организовано так, что будет патчится только QuerySet на котором был вызван nested_values
+    На вход принимаем именно объект, а не функцию, так как на данный момент метод класса уже прикреплён
+    к конкретному экземпляру класса.
+    """
+    # запоминаем исходную функцию копирования, для переданного экземпляра
+    _clone = old_obj._clone
+
+    @wraps(_clone)
+    def wrapper(**kwargs):
+        new_obj = _clone(**kwargs)
+        # в новый объект переносим копию _nested_values
+        new_obj._nested_values = deepcopy(getattr(old_obj, '_nested_values', None))
+        # и также патчим функцию копирования
+        new_obj._clone = clone_wrapper(new_obj)
+        return new_obj
+    return wrapper
+
+
+def _nested_values(self, fieldsmap, nest=None) -> QuerySet:
+    nv = NV(fieldsmap=fieldsmap, nest=nest)
+    clone = self._values(*nv.values_list)
+    clone._iterable_class = NestedValuesIterable
+    clone._nested_values = nv
+    clone._clone = clone_wrapper(clone)
+    return clone
+
+
+def setup():
+    """Инициализируем модуль"""
+    QuerySet.nested_values = _nested_values
